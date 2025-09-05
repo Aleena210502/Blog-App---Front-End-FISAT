f8712d4f3f42ebf0a7690393c264e7ff02ac9815
Aleena Joshy <joshy200467@gmail.com>
2025-09-05 16:32:13 +0530

    create post completed

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const CreatePost = () => {
+
+  const [token,setToken]=useState(sessionStorage.getItem("token"))
+
+  const [input,setInput]=useState(
+    {"Message":""}
+  )
+
+  const inputHandler=(event)=>{
+        setInput({...input,[event.target.name]:event.target.value})
+    }
+
+  const readValues=()=>{
+    console.log(input)
+    console.log(token)
+    axios.post("http://localhost:3030/create",input, {
+      headers:{"token":token,"Content-Type":"application/json"}
+    }).then(
+      (response)=>{
+        console.log(response.data)
+
+if (response.data.status=="success") {
+  alert("Posted Successfully")
+  
+} else {
+  alert("Something went Wrong !!! ")
+  
+}
+
+      }
+    ).catch(
+      (error)=>(console.log(error))
+    )
+  }
+
+
   return (
     <div>
 
@@ -10,13 +46,13 @@ const CreatePost = () => {
         <div className="row g-3">
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="label form-label">Post a Message</div>
-            <textarea name="" id="" className="form-control"></textarea>
+            <textarea name="Message" value={input.Message} className="form-control" onChange={inputHandler}></textarea>
             
 
 
           </div>
           <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <button className="btn btn-success">Post</button>
+            <button onClick={readValues} className="btn btn-success">Post</button>
 
           </div>
         </div>
